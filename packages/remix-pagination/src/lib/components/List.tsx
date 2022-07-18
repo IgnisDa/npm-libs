8c9e84f7ff5a0608dd177be224f8768b7a31e9c0
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-07-18 07:11:32 +0000

    fix(remix-pagination): add information for list component

--- packages/remix-pagination/src/lib/components/List.tsx
+++ packages/remix-pagination/src/lib/components/List.tsx
@@ -5,6 +5,9 @@ interface ListProps {
   prefix: string;
 }
 
+/**
+ * This component is used to display a list of unordered elements
+ */
 export const List: FC<ListProps> = ({ children, prefix }) => {
   return <ul className={`${prefix}remix-pagination__list`}>{children}</ul>;
 };
